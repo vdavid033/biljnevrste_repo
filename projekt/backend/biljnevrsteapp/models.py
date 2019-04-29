880a5f449a238fbf996a36b572d22baac06a33fe
MariaMispi <48434065+MariaMispi@users.noreply.github.com>
2019-04-29 16:52:24 +0200

    test

---
+++ projekt/backend/biljnevrsteapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class BiljneVrste(models.Model):
+    naziv = models.CharField(max_length=100)
+    vrsta = models.CharField(max_length=100)
+    sinonim = models.CharField(max_length=100, default='nesto')
+    opis = models.CharField(max_length=100, default='opis')
