682d206d190389a7d73dbe73502ff68ccac2e587
Ivan Miljančić <ivan.miljancic@gmail.com>
2019-05-01 23:10:23 +0200

    Created REST models and REST routes

--- projekt/backend/biljnevrste/biljnevrste/urls.py
+++ projekt/backend/biljnevrste/biljnevrste/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('biljnevrsteapp.urls')),
+    path('api/', include('biljnevrsteapp.urls')),
     path('admin/', admin.site.urls),
 ]
