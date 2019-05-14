4bf36434b4a72aef173f3248b5300fc982c1c127
MariaMispi <48434065+MariaMispi@users.noreply.github.com>
2019-05-14 19:53:00 +0200

    14.05

---
+++ projekt/frontend/maria2/src/pages/Play.vue
@@ -0,0 +1,31 @@
+<template>
+    <q-page padding="">
+        <div class="row gutter-xs">
+            <div class="col">
+                Maria test maria test
+                <div>
+                    <q-btn round color="primary" label="Primary" @click="handleClick" />
+                    <q-input outlined v-model="myInput" label="Outlined" />
+                </div>
+            </div>
+            <div class="col">
+                Maria test maria test
+            </div>
+        </div>
+    </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      myInput: ''
+    }
+  },
+  methods: {
+    handleClick () {
+      this.myInput = 'clicked'
+    }
+  }
+}
+</script>
