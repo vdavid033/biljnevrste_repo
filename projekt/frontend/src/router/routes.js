0862d9ef6ddab3e5f877dd85f6b1296b496d89c9
tio-nio <antonio@bodul.com>
2019-05-30 20:34:01 +0200

    napravljen layout i prazni pageovi

---
+++ projekt/frontend/src/router/routes.js
@@ -0,0 +1,40 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') }
+    ]
+  },
+  {
+    path: '/unos',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/unos.vue') }
+    ]
+  },
+  {
+    path: '/kviz',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/kviz.vue') }
+    ]
+  },
+  {
+    path: '/pregled',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/pregled.vue') }
+    ]
+  }
+]
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
