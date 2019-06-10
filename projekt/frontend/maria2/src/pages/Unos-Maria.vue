90aff64b543186e3a5fccc42e2b90dead8834d54
MariaMispi <48434065+MariaMispi@users.noreply.github.com>
2019-06-10 23:52:32 +0200

    Posljednji unos - 10.06.

--- projekt/frontend/maria2/src/pages/Unos-Maria.vue
+++ projekt/frontend/maria2/src/pages/Unos-Maria.vue
@@ -4,81 +4,90 @@
             <div class="col">
                 Hrvatski naziv:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="hrnaziv" label="Hrvatski naziv" />
                 </div>
             </div>
             <div class="col">
                 Sinonim:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="sinonim" label="Sinonim" />
                 </div>
             </div>
         </div>
         <div class="row gutter-xs">
             <div class="col">
                 Latinski naziv:
+                <div>
+                  <q-input outlined v-model="latnaziv" label="Latinski naziv" />
+                </div>
             </div>
             <div class="col">
                 Rod:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="rod" label="Rod" />
                 </div>
             </div>
             <div class="col">
                 <div class="col">
                 Vrsta:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="vrsta" label="Vrsta" />
                 </div>
             </div>
             </div>
             <div class="col">
                 Podvrsta-ssp.:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="podvrsta" label="Podvrsta" />
                 </div>
             </div>
             <div class="col">
                 Varijetet:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="varijetet" label="Varijetet" />
                 </div>
             </div>
             <div class="col">
                 <div class="col">
                 Sistemtičari:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="sistem" label="Sistematičar" />
                 </div>
             </div>
           </div>
           </div>
-        <div class="row gutter-xs">
-            <div class="col">
-                <div>
-                <q-btn-dropdown label="Botanička porodica:">
-                  <!-- dropdown content -->
-                  <q-list link>
-                    <q-item>
-                      <q-item-main>
-                        <q-item-tile>Item</q-item-tile>
-                      </q-item-main>
-                    </q-item>
-                  </q-list>
-                </q-btn-dropdown>
-                </div>
-            </div>
+    <div class="q-pa-lg row">
+      <!--vd -->
+      <div class="col">
+        <q-list bordered padding class="rounded-borders" style="max-width: 300px">
+        <q-item-label header>Izaberi botaničku porodicu</q-item-label>
+        <q-item v-for="porodica in porodice" :key="porodica.id" class="q-my-sm" clickable v-ripple>
+          <q-item-section>
+            <q-radio v-model="radioR" :val=porodica.naziv_roda />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>
+              <a :href=porodica.url>
+              {{ porodica.hrvatski_naziv_porodice }}
+              </a>
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+        </q-list>
+      </div>
+      <!-- end vd -->
+    </div>
             <div class="col">
                 Dodaj novu lat. kat.:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="latkat" label="Lat. kategorija" />
                 </div>
             </div>
             <div class="col">
                 <div class="col">
                 Dodaj novu hrv. kat.:
                 <div>
-                    <q-input outlined v-model="myInput" label="Outlined" />
+                    <q-input outlined v-model="hrvkat" label="Hr. kategorija" />
                 </div>
             </div>
             </div>
@@ -87,7 +96,6 @@
                   <q-btn round color="primary" label="+" text-color="black" @click="handleClick" />
               </div>
           </div>
-        </div>
         <div class="row gutter-xs">
           <div class="col">
               Uporabni dio:
@@ -95,14 +103,14 @@
           <div class="col">
               Bioaktivna tvar:
               <div>
-                  <q-input outlined v-model="myInput" label="Outlined" />
+                  <q-input outlined v-model="bioakt" label="Bioaktivna tvar" />
               </div>
           </div>
           <div class="col">
               <div class="col">
               Opis biljke:
               <div>
-                  <q-input outlined v-model="myInput" label="Outlined" />
+                  <q-input outlined v-model="opis" label="Oopis" />
               </div>
           </div>
           </div>
@@ -147,12 +155,19 @@
             <div class="q-gutter-sm">
               <q-checkbox v-model="zadebljalikorjen" label="zadebljali korjen" />
             </div>
-            <div class="q-gutter-sm">
-              <q-checkbox v-model="teal" label="dodaj" />
-            </div>
-
             <div class="q-px-sm">
             </div>
+          </div>
+          <div class="col">
+              Dodaj novi uporabni dio:
+              <div>
+                  <q-input outlined v-model="uporab" label="Uporabni dio" />
+              </div>
+          </div>
+          <div class="col">
+              <div>
+                  <q-btn round color="primary" label="+" text-color="black" @click="handleClick" />
+              </div>
           </div>
         <div class="row gutter-xs">
           <div class="col">
@@ -170,8 +185,13 @@
           </div>
         <div class="row gutter-xs">
           <div class="col">
+          <div class="q-pa-lg text-weight-bold q-display-2">
+            {{this.rod.naziv_roda}}
+          </div>
               <div>
                   <q-btn color="white" text-color="black" label="SPREMI" />
+              </div>
+              <div>
               </div>
           </div>
         </div>
@@ -180,21 +200,23 @@
 </template>
 
 <script>
+
 export default {
-  loadData () {
-    this.$axios.get('http://193.198.97.14:8000/api/porodice/1/?format=api')
-      .then((response) => {
-        this.data = response.data
-      })
-      .catch(() => {
-        this.$q.notify({
-          message: 'Loading failed'
-        })
-      })
-  },
   data () {
     return {
-      myInput: '',
+      hrnaziv: '',
+      sinonim: '',
+      latnaziv: '',
+      rod: '',
+      vrsta: '',
+      podvrsta: '',
+      varijetet: '',
+      sistematicar: '',
+      latkat: '',
+      hrvkat: '',
+      bioaktv: '',
+      uporab: '',
+      opis: '',
       text: '',
       herba: true,
       cvat: true,
@@ -210,18 +232,27 @@ export default {
       sjeme: true,
       zadebljalikorjen: true,
       loading: true,
-      post: []
-
+      sistem: '',
+      radioR: '',
+      porodice: []
     }
   },
   methods: {
-    handleClick () {
-      this.myInput = 'clicked'
+    onItemClick () {
+      console.log('Clicked on an Item')
+    },
+    fetchBiljneVrste () {
+      this.$axios.get('http://193.198.97.14:8000/api/porodice/?format=json')
+        .then((response) => {
+          this.porodice = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
+  },
+  created () {
+    this.loadData()
   }
 }
 </script>
-
-  function newFunction() {
-    return null;
-  }
