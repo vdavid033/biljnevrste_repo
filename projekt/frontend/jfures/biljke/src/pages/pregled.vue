e407d3da2526287c3397177225651b1370429529
Josip Fures <jfures94@gmail.com>
2019-06-25 10:38:16 +0200

    Pozicioniranje tablice na stranici
    Tablica pozicionirana desno od uporabnih dijelova. Isto tako riješene sve greške pronađene kroz consolu u browseru

--- projekt/frontend/jfures/biljke/src/pages/pregled.vue
+++ projekt/frontend/jfures/biljke/src/pages/pregled.vue
@@ -1,11 +1,14 @@
 <template>
     <q-page padding="">
+        <div class="row">
         <div class="absolute" style="top: 0; right: 20px; transform: translateY(50%);">
           <label>Bioaktivna tvar: </label>
-          <input type="text" v-model="search" placeholder="search" />
+          <input class="form-control" type="text" v-model="search" placeholder="Search" />
         </div>
-          <div class="q-pa-md">
-              <q-label>Uporabni dio</q-label>
+            <div class="row">
+        <q-list bordered padding class="rounded-borders" style="max-width: 300px">
+        <q-item-label header>Uporabni dio</q-item-label>
+        <div class="q-pa-md">
             <div class="q-gutter-sm">
               <q-checkbox v-model="herba" label="herba" />
             </div>
@@ -48,56 +51,85 @@
             <div class="q-px-sm">
             </div>
           </div>
-        <table class="q-table responsive">
-  <thead>
-    <tr>
-      <th>Botanička porodica (lat.)</th>
-      <th>Rod (lat.)</th>
-      <th>Porodica (hrv.)</th>
-        <th>Vrsta</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td class="text-center">Botanička porodica #1</td>
-      <td class="text-center">1</td>
-      <td class="text-center">1</td>
-      <td class="text-center">1</td>
-    </tr>
-    <tr>
-     <td class="text-center">Botanička porodica #2</td>
-      <td class="text-center">2</td>
-      <td class="text-center">2</td>
-      <td class="text-center">2</td>
-    </tr>
-    <tr>
-      <td class="text-center">Botanička porodica #3</td>
-      <td class="text-center">3</td>
-      <td class="text-center">3</td>
-      <td class="text-center">3</td>
-    </tr>
-    <tr>
-      <td class="text-center">Botanička porodica #4</td>
-      <td class="text-center">4</td>
-      <td class="text-center">4</td>
-      <td class="text-center">4</td>
-    </tr>
-  </tbody>
-</table>
-<div class="row gutter-xs">
-          <div class="col">
-              <div>
-                  <q-btn color="blue" text-color="black" class="absolute" style="right: 0; right: 800px; transform: translateY(50%);" label="PRIKAZ" />
-              </div>
-          </div>
-        </div>
+        <q-item  v-for="dio in uporabnidijelovi" :key="dio.id" class="q-my-sm" clickable v-ripple>
+          <q-item-section>
+            <q-item-label title>{{ dio.naziv }}</q-item-label>
+          </q-item-section>
+        </q-item>
+        </q-list>
+      </div>
+      <div style="transform: translate(50%,20%);">
+    <table class="q-table responsive">
+      <thead>
+        <tr>
+          <th class="text-left">Vrsta</th>
+          <th class="text-left">Rod (lat.)</th>
+          <th class="text-left">Porodica (hrv.)</th>
+          <th class="text-left">Botanička porodica (lat.)</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="vrsta in biljnevrste" :key="vrsta.id" class="q-my-sm" clickable v-ripple>
+          <td class="text-left">
+            <q-item-section><q-item-label><a :href=vrsta.url>{{ vrsta.hrvatski_naziv_vrste }}</a></q-item-label></q-item-section>
+          </td>
+          <td class="text-left">
+            <span  v-for="rod in rodovi" :key="rod.id" class="q-my-sm" clickable v-ripple>
+              <span v-if="rod.url === vrsta.ID_roda">
+                <q-item-label>
+                  <a :href=rod.url>
+                  {{ rod.naziv_roda }}
+                  </a>
+                </q-item-label>
+              </span>
+            </span>
+          </td>
+          <td class="text-left">
+            <span v-for="rod in rodovi" :key="rod.id" class="q-my-sm" v-ripple>
+              <span v-if="rod.url === vrsta.ID_roda">
+                <span v-for="porodica in porodice" :key="porodica.ID_roda" class="q-my-sm" clickable v-ripple>
+                  <span v-if="rod.ID_porodice === porodica.url">
+              <q-item-section>
+                <q-item-label>
+                  <a :href=porodica.url>
+                  {{ porodica.hrvatski_naziv_porodice }}
+                  </a>
+                </q-item-label>
+              </q-item-section>
+              </span>
+              </span>
+              </span>
+            </span>
+          </td>
+          <td class="text-left">
+            <span v-for="rod in rodovi" :key="rod.id" class="q-my-sm" clickable v-ripple>
+              <span v-if="rod.url === vrsta.ID_roda">
+                <span v-for="porodica in porodice" :key="porodica.ID_roda" class="q-my-sm" clickable v-ripple>
+                  <span v-if="rod.ID_porodice === porodica.url">
+              <q-item-section>
+                <q-item-label>
+                  <a :href=porodica.url>
+                  {{ porodica.latisnki_naziv_porodice }}
+                  </a>
+                </q-item-label>
+              </q-item-section>
+              </span>
+              </span>
+              </span>
+            </span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+    </div>
     </q-page>
 </template>
 
 <script>
 export default {
   loadData () {
-    this.$axios.get('http://193.198.97.14:8000/api/porodice/1/?format=api')
+    this.$axios.get('http://193.198.97.14:8000/api/porodice/')
       .then((response) => {
         this.data = response.data
       })
@@ -111,6 +143,7 @@ export default {
     return {
       myInput: '',
       text: '',
+      radioU: '',
       herba: true,
       cvat: true,
       korjen: true,
@@ -124,14 +157,63 @@ export default {
       gomolj: true,
       sjeme: true,
       zadebljalikorjen: true,
-      post: []
-
+      post: [],
+      search: '',
+      rodovi: [],
+      biljnevrste: [],
+      porodice: [],
+      uporabnidijelovi: []
     }
   },
+  created () {
+    this.fetchRodovi()
+    this.fetchBiljneVrste()
+    this.fetchPorodice()
+  },
   methods: {
-    handleClick () {
-      this.myInput = 'clicked'
+    fetchRodovi () {
+      this.$axios.get('http://193.198.97.14:8000/api/rodovi/?format=json')
+        .then((response) => {
+          this.rodovi = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    fetchBiljneVrste () {
+      this.$axios.get('http://193.198.97.14:8000/api/biljnevrste/?format=json')
+        .then((response) => {
+          this.biljnevrste = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    fetchPorodice () {
+      this.$axios.get('http://193.198.97.14:8000/api/porodice/?format=json')
+        .then((response) => {
+          this.porodice = response.data
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
+  },
+  fetchUporabniDijelovi () {
+    this.$axios.get('http://193.198.97.14:8000/api/uporabnidijelovi/?format=json')
+      .then((response) => {
+        this.uporabnidijelovi = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+  randomLink (e, go) {
+    e.navigate = false
+    // this.kvizLink = this.links[Math.floor(Math.random() * 2)]
+    setTimeout(() => {
+      go()
+    }, 1)
   }
 }
 </script>
